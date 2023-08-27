cda982f864964abdedb0e3827f8af5d6d8f9e90c
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-08-27 18:28:31 +0530

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-async function fetchData(){
-    const url = 'https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=csv&unitGroup=us&shortColumnNames=0';
+async function weatherData(){
+    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=bangalore';
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '574441b6efmshabb3656029fabcap174e9ejsnd619ddb478a8',
-            'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
         }
     };
     
@@ -13,12 +12,8 @@ async function fetchData(){
         const response = await fetch(url, options);
         const result = await response.text();
         console.log(result);
-        // document.getElementById('Address')=result.content
-        const csvDataElement = document.getElementById('csvData');
-        csvDataElement.innerText = result;
     } catch (error) {
         console.error(error);
     }
 }
-
-fetchData()
+weatherData()
